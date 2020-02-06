460d521b597ce70efce4a217d06231c3068d2a71
Yevgeniy Borisenko <yevgeniy.borisenko@nearshorepartner.ch>
2020-02-06 18:20:16 +0200

    Updates

---
+++ precache-manifest.48b33546f498320bc7dd0dcbdb0381f8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4facf9183fff2dbcd0da43abb0132eb0",
+    "url": "/Speicherbox/index.html"
+  },
+  {
+    "revision": "763c4588f06987d4181b",
+    "url": "/Speicherbox/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "cc3aea24c165fdc53aa3",
+    "url": "/Speicherbox/static/css/main.c84bd245.chunk.css"
+  },
+  {
+    "revision": "763c4588f06987d4181b",
+    "url": "/Speicherbox/static/js/2.ea8606d6.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/Speicherbox/static/js/2.ea8606d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc3aea24c165fdc53aa3",
+    "url": "/Speicherbox/static/js/main.e8f86e0a.chunk.js"
+  },
+  {
+    "revision": "a88f1f69983fe2826172",
+    "url": "/Speicherbox/static/js/runtime-main.ab5d9aba.js"
+  },
+  {
+    "revision": "88de5b0302ec7701b69cc76579663b21",
+    "url": "/Speicherbox/static/media/Datensicherheit, Tresor_smal.88de5b03.jpg"
+  },
+  {
+    "revision": "64db3485f3082d6ee1b89cc953833975",
+    "url": "/Speicherbox/static/media/business.64db3485.jpg"
+  },
+  {
+    "revision": "ed5201b0d9ea88eefbda5fb18a1bce1b",
+    "url": "/Speicherbox/static/media/business_small.ed5201b0.jpg"
+  },
+  {
+    "revision": "27d93d7c83bcbdd3d21c97c4194a4be1",
+    "url": "/Speicherbox/static/media/family.27d93d7c.jpg"
+  },
+  {
+    "revision": "25a13355c0f1cff58f74b5ca63b6ac4a",
+    "url": "/Speicherbox/static/media/family_small.25a13355.jpg"
+  },
+  {
+    "revision": "a6e4ab7fe665475104380637381991c4",
+    "url": "/Speicherbox/static/media/home.a6e4ab7f.jpg"
+  },
+  {
+    "revision": "3ee1616ebc3485634f03432e1a55bf34",
+    "url": "/Speicherbox/static/media/home_small.3ee1616e.jpg"
+  },
+  {
+    "revision": "865fa3852d99b8f80a07b7d123161a1f",
+    "url": "/Speicherbox/static/media/logo_Wuerfel_speicherbox_1_klein.865fa385.png"
+  },
+  {
+    "revision": "36329283bfe0d525d0325a010337e191",
+    "url": "/Speicherbox/static/media/reseller.36329283.jpg"
+  },
+  {
+    "revision": "bae71b34192d504f41d7edd9dcd47ce3",
+    "url": "/Speicherbox/static/media/reseller_small.bae71b34.jpg"
+  },
+  {
+    "revision": "df770f2ab6d557d645ffffadf9a76c3b",
+    "url": "/Speicherbox/static/media/swissmade.df770f2a.png"
+  },
+  {
+    "revision": "1f17fe7aaba113b56e5f2237ed6eaab7",
+    "url": "/Speicherbox/static/media/verein_small.1f17fe7a.jpg"
+  },
+  {
+    "revision": "c52aff27cceff30cdc72dfb37a280631",
+    "url": "/Speicherbox/static/media/vereine.c52aff27.jpg"
+  }
+]);
